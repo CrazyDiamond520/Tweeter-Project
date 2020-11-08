4f7c86066cb6340c3c9487927b740df501d0e4ed
Amit Peleg <amitpeleg520@gmail.com>
2020-11-08 10:31:27 +0200

    added some styling

--- render.js
+++ render.js
@@ -8,16 +8,16 @@ const Renderer = function(){
             const div = $(`<div class="post" data-postId="${id}"></div>`)
             // console.log(id)
             div.append(`<div class="post" data-postId="${id}"> ${posts[i].text} </div>`)
-            div.append(`<div class="delete" data-postId="${id}">delete post</div>`)
+            div.append(`<div class="delete" data-postId="${id}"><i class="fas fa-trash"></i></div>`)
             for(let j in posts[i].comments){
                 let commentid = posts[i].comments[j].id
                 let comment = posts[i].comments[j].text
                 // console.log(comment)
-                div.append(`<div class="comments" data-commentId="${commentid}"><span class="delete-comment">x</span>${comment} </div>`)
+                div.append(`<div class="comments" data-commentId="${commentid}"><span class="delete-comment"><i class="far fa-trash-alt"></i></span>${comment} </div>`)
                 div.append()
             }
-            div.append(`<input type="text" class="newComment" placeholder="insert comment">`)
-            div.append(`<div class="addComment" data-postId="${id}">Add Comment</div>`)
+            div.append(`<input type="text" class="newComment" placeholder="insert comment"><span class="addComment" data-postId="${id}">Add Comment</span>`)
+            
             $("#posts").append(div)
         }
 
